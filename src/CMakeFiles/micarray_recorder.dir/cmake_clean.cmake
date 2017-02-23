069eb6e5a64623ad2bf43791c7f107431553c7e1
John Sarracino <jsarraci@cs.ucsd.edu>
2017-02-22 20:59:02 -0800

    initial commit of matrix-creator-hal and mic demo

---
+++ src/CMakeFiles/micarray_recorder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/micarray_recorder.dir/micarray_recorder.cpp.o"
+  "micarray_recorder.pdb"
+  "micarray_recorder"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/micarray_recorder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/micarray_recorder.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for micarray_recorder.
+# This may be replaced when dependencies are built.
