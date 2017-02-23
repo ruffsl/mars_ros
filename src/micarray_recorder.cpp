42f8074ec92d25edc3d645813ed0e02a3eb14b91
John Sarracino <jsarraci@cs.ucsd.edu>
2017-02-22 21:06:20 -0800

    cut micarray down to size

--- src/micarray_recorder.cpp
+++ src/micarray_recorder.cpp
@@ -10,10 +10,8 @@
 #include <iostream>
 #include <valarray>
 
-#include "../cpp/driver/everloop_image.h"
-#include "../cpp/driver/everloop.h"
-#include "../cpp/driver/microphone_array.h"
-#include "../cpp/driver/wishbone_bus.h"
+#include "../matrix-creator-hal/cpp/driver/microphone_array.h"
+#include "../matrix-creator-hal/cpp/driver/wishbone_bus.h"
 
 namespace hal = matrix_hal;
 
@@ -24,14 +22,7 @@ int main() {
   hal::MicrophoneArray mics;
   mics.Setup(&bus);
 
-  hal::Everloop everloop;
-  everloop.Setup(&bus);
-
-  hal::EverloopImage image1d;
-
   for (auto& led : image1d.leds) led.red = 10;
-
-  everloop.Write(&image1d);
 
   uint16_t seconds_to_record = 10;
 
@@ -52,22 +43,6 @@ int main() {
     }
     if (step == seconds_to_record * mics.SamplingRate()) break;
   }
-
-  for (uint16_t c = 0; c < mics.Channels() + 1; c++) {
-    std::string filename = "mic_" + std::to_string(mics.SamplingRate()) +
-                           "_s16le_channel_" + std::to_string(c) + ".raw";
-    std::ofstream os(filename, std::ofstream::binary);
-    os.write((const char*)buffer[c],
-             seconds_to_record * mics.SamplingRate() * sizeof(int16_t));
-
-    os.close();
-  }
-
-  for (auto& led : image1d.leds) {
-    led.red = 0;
-    led.green = 10;
-  }
-  everloop.Write(&image1d);
 
   return 0;
 }
